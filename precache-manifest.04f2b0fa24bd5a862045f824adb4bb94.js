a8bd93522bebb5dd03ebf8579cdc824e5aa0d21a
Gabriel Marquez <gaabmarquez@gmail.com>
2020-02-12 11:37:20 -0300

    Updates

---
+++ precache-manifest.04f2b0fa24bd5a862045f824adb4bb94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8e5817ee1f273f2403e280f290f26d",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "c2302bd864c53bc78aba",
+    "url": "/trello-clone/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f7da363c6a23da97f923",
+    "url": "/trello-clone/static/css/main.1791526a.chunk.css"
+  },
+  {
+    "revision": "c2302bd864c53bc78aba",
+    "url": "/trello-clone/static/js/2.5e41d94e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/trello-clone/static/js/2.5e41d94e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7da363c6a23da97f923",
+    "url": "/trello-clone/static/js/main.90646cc1.chunk.js"
+  },
+  {
+    "revision": "01d9cced60e505c9249c",
+    "url": "/trello-clone/static/js/runtime-main.17cd50a1.js"
+  }
+]);
