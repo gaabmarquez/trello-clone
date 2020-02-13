83a0a4370a795945418a00d68243c14e1382ebfe
Gabriel Marquez <gaabmarquez@gmail.com>
2020-02-13 02:16:33 -0300

    Removed unnecesary Card styled component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,45 +1,25 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
-import styled from 'styled-components';
-
-const CardContainer = styled.div`
-margin: 10px;
-boxsizing: border-box;
-border-radius: 3px;
-height: auto;
-word-wrap: break-word;
-`;
 
 const Card = ({ id, text, index }) => {
- 
-
   return (
     <Draggable draggableId={String(id)} index={index}>
       {provided => (
-        <CardContainer>
-          <div
-            className='card'
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-          >
-            <div className='card-body' style={styles.cardBody}>
-              {text}
-            </div>
+        <div
+          className='card mb-2 mx-2'
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div className='card-body' style={styles.cardBody}>
+            {text}
           </div>
-        </CardContainer>
+        </div>
       )}
     </Draggable>
   );
 };
 const styles = {
-  card: {
-    margin: '20px',
-    boxSizing: 'border-box',
-    borderRadius: '3px',
-    height: 'auto',
-    wordWrap: 'break-word'
-  },
   cardBody: {
     padding: '10px',
     fontSize: '14px'
