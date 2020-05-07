e7358d1f987fdf5812329a29d4d4829c29984cc8
Gabriel Marquez <gaabmarquez@gmail.com>
2020-05-07 12:27:39 -0300

    Added footer copyright

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,16 @@ import './App.scss';
 import BoardContainer from './board/BoardContainer';
 
 function App() {
-  return <BoardContainer />;
+  return (
+    <>
+      <BoardContainer />
+      <div className='footer d-flex justify-content-center'>
+        <a target='_blank' rel="noopener noreferrer"  href='http://www.linkedin.com/in/gaabmarquez'>
+          © 2020, Created by Gabriel Márquez
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default App;
